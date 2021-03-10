73bd2bdb8b1ca9286de2c5cff48c26ab4a53a16a
franco@mumuki.org <franco@mumuki.org>
2021-03-10 14:35:23 +0000

    Mumuki Export on 2021-03-10 14:35:23 +0000

--- 00013_Empezando el día/test.py
+++ 00013_Empezando el día/test.py
@@ -1,4 +1,7 @@
 class Test(unittest.TestCase):
 
-  def test_description_example(self):
-    self.assertTrue(True)
+  def test_saludar_a_gus(self):
+    self.assertEquals(saludar_a("Gus"), "Buenos días Gus")
+    
+  def test_saludar_a_may(self):
+    self.assertEquals(saludar_a("May"), "Buenos días May")
