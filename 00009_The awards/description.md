7418cf24abd76ac83f8d9478e2192046675decaa
fedescarpa@mumuki.org <fedescarpa@mumuki.org>
2023-03-22 21:15:00 +0000

    Mumuki Export on 2023-03-22 21:15:00 +0000

---
+++ 00009_The awards/description.md
@@ -0,0 +1,15 @@
+The jury of a tournament asked us to develop a function `medal_for` that returns the medal for to the first places, according to the following logic:
+
+* 🥇 first place: it corresponds to `"gold"`;
+* 🥈 second place: it corresponds to `"silver"`;
+* 🥉 third place: it corresponds to `"bronze"`;
+* 😅 any other place: corresponds to `"nothing"`
+
+```python
+ムmedal_for(1)
+"gold"
+ムmedal_for(5)
+"nothing"
+```
+
+> Define the function `medal_for` and test it in the console.
---
+++ 00009_The awards/test.py
@@ -0,0 +1,20 @@
+
+  def test_medal_for_1(self):
+    self.assertEqual(medal_for(1), "gold")
+
+  def test_medal_for_2(self):
+    self.assertEqual(medal_for(2), "silver")
+
+  def test_medal_for_3(self):
+    self.assertEqual(medal_for(3), "bronze")
+
+  def test_medal_for_4(self):
+    self.assertEqual(medal_for(4), "nothing")
+
+  def test_medal_for_5(self):
+    self.assertEqual(medal_for(5), "nothing")
+
+  def test_medal_for_0(self):
+    self.assertEqual(medal_for(0), "nothing")
+    
+    
