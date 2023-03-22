f81c62f258433c99bf8697c2c13c91b55af35dba
fedescarpa@mumuki.org <fedescarpa@mumuki.org>
2023-03-22 21:10:17 +0000

    Mumuki Export on 2023-03-22 21:10:17 +0000

--- 00008_What's your sign_/test.py
+++ 00008_What's your sign_/test.py
@@ -1,17 +1,17 @@
 
-  def test_signo_10_es_1(self):
-    self.assertEqual(signo(10), 1)
+  def test_sign_10_is_1(self):
+    self.assertEqual(sign(10), 1)
 
-  def test_signo_1_es_1(self):
-    self.assertEqual(signo(1), 1)
+  def test_sign_1_is_1(self):
+    self.assertEqual(sign(1), 1)
 
-  def test_signo_0_es_0(self):
-    self.assertEqual(signo(0), 0)
+  def test_sign_0_is_0(self):
+    self.assertEqual(sign(0), 0)
 
-  def test_signo_menos_65_es_menos_1(self):
-    self.assertEqual(signo(-65), -1)
+  def test_sign_negative_65_is_negative_1(self):
+    self.assertEqual(sign(-65), -1)
 
-  def test_signo_menos_87_es_menos_1(self):
-    self.assertEqual(signo(-87), -1)
+  def test_sign_negative_87_is_negative_1(self):
+    self.assertEqual(sign(-87), -1)
 
 
