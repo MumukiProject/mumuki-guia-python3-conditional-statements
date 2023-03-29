27a165b4da72b388c2cec3efa08bbdb296dc961d
franco@mumuki.org <franco@mumuki.org>
2023-03-29 16:46:54 +0000

    Mumuki Export on 2023-03-29 16:46:53 +0000

--- 00006_More badges/test.py
+++ 00006_More badges/test.py
@@ -1,13 +1,13 @@
   
-  def test_write_badge_Ana_Perez_false(self):
+  def test_write_badge_Ana_Perez_false_must_write_a_short_badge(self):
     self.assertEqual(write_badge("Ana", "Perez", "Backend Developer", False), "Ana Perez")
   
-  def test_write_badge_Julio_Gelman_false(self):
+  def test_write_badge_Julio_Gelman_false_must_write_a_short_badge(self):
     self.assertEqual(write_badge("Julio", "Gelman", "Helpdesk", False), "Julio Gelman")
   
-  def test_write_badge_Ana_Perez_true(self):
+  def test_write_badge_Ana_Perez_true_must_write_a_long_badge(self):
     self.assertEqual(write_badge("Ana", "Perez", "Backend Developer", True), "Ana Perez, Backend Developer")
   
-  def test_write_badge_Julio_Gelman_true(self):
+  def test_write_badge_Julio_Gelman_true_must_write_a_long_badge(self):
     self.assertEqual(write_badge("Julio", "Gelman", "Helpdesk", True), "Julio Gelman, Helpdesk")
   
